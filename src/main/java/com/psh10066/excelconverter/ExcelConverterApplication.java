e33b2f18fd31c998359ac8674c98ce9dab81cfa4
이호준 <psh10066@gmail.com>
2024-05-03 17:34:41 +0900

    프로젝트 생성

---
+++ src/main/java/com/psh10066/excelconverter/ExcelConverterApplication.java
@@ -0,0 +1,13 @@
+package com.psh10066.excelconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelConverterApplication.class, args);
+    }
+
+}
